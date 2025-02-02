7252f9eb8393107eba91e0f74f9b1d67dda37259
popiemon <kryo.over@gmail.com>
2025-02-02 10:25:15 +0000

    add: main

--- run/main.py
+++ run/main.py
@@ -1,9 +1,23 @@
+import os
+
+from tqdm import tqdm
+
 from ollama_rag.chatpdf import ChatPDF
 
 
 def main():
     chatpdf = ChatPDF()
-    chatpdf.ingest("/workspace/data/GaN/1.126940.pdf")
+    pdf_dir = "/workspace/data/GaN"
+    for filename in tqdm(os.listdir(pdf_dir)):
+        if filename.endswith(".pdf"):
+            try:
+                chatpdf.ingest(os.path.join(pdf_dir, filename))
+            except Exception as e:
+                print(f"Error processing {filename}: {e}")
+                print("Continuing to the next document...")
+                print("file:, ", filename)
+                continue
+
     print(chatpdf.ask("What is GaN?"))
 
 
